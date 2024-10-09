2d11d11aca80a611bc58a4430cd7a741c66dd5c3
Colton Doling <cmdoling@gmail.com>
2024-10-09 19:26:16 -0400

    added weather data endpoint and fixed some javascript bugs

--- assets/src/main.js
+++ assets/src/main.js
@@ -1,4 +1,4 @@
-const userLocation = document.getElementById(userLocation),;
+const userLocation = document.getElementById('userLocation'),
 converter = document.getElementById("converter"),
 weatherIcon = document.querySelector(".weatherIcon"),
 temperature = document.querySelector(".temperature"),
@@ -7,7 +7,7 @@ description = document.querySelector(".description"),
 date = document.querySelector(".date"),
 city = document.querySelector(".city"),
 HValue = document.getElementById("HValue"),
-WValue = document.getElemenById("WValue"),
+WValue = document.getElementById("WValue"),
 SRValue = document.getElementById("SRValue"),
 SSValue = document.getElementById("SSvalue"),
 CValue = document.getElementById("CValue"),
@@ -15,8 +15,8 @@ UVValue = document.getElementById("UVValue"),
 PValue = document.getElementById("PValue"),
 Forecast = document.querySelector(".Forecast");
 
-WEATHER_API_ENDPOINT='https://api.openweathermap.org/data/2.5/weather?appid=';
-WEATHER_DATA_ENDPOINT='';
+WEATHER_API_ENDPOINT='https://api.openweathermap.org/data/2.5/weather?appid=3b4270389f20c03b9af3d0d9bc2619ae&q=';
+WEATHER_DATA_ENDPOINT='https://api.openweathermap.org/data/2.5/weather?appid=3b4270389f20c03b9af3d0d9bc2619ae&exclude=minutely&units=metric&';
 
 function findUserLocation() {
   fetch(WEATHER_API_ENDPOINT + userLocation.value)
@@ -26,14 +26,15 @@ function findUserLocation() {
          alert(data.message);
          return;
     }
-console.log(data);
+    console.log(data);
 
-city.innerHTML = data.name + ", " + data.sys.country;
+    city.innerHTML = data.name + ", " + data.sys.country;
     fetch(
       WEATHER_DATA_ENDPOINT + `lon=${data.coord.lon}&lat=${data.coord.lat}`)
-  )
-  .then((response)=> response.json())
-  .then((data)=> {
-    console.log(data);
+    
+    .then((response)=> response.json())
+    .then((data)=> {
+      console.log(data);
+    });
   });
-}
+};
