e897feb47514cca7eec60c0695ede7673dc86e1e
Colton Doling <cmdoling@gmail.com>
2024-10-10 19:26:27 -0400

    fixed several javascript bugs

--- assets/src/main.js
+++ assets/src/main.js
@@ -1,22 +1,22 @@
-const userLocation = document.getElementById("userLocation"),
+userLocation = document.getElementById("userLocation")
 converter = document.getElementById("converter"),
 weatherIcon = document.querySelector(".weatherIcon"),
 temperature = document.querySelector(".temperature"),
-feelsLike = document.querySelector("feelsLike"),
+feelsLike = document.querySelector(".feelsLike"),
 description = document.querySelector(".description"),
 date = document.querySelector(".date"),
 city = document.querySelector(".city"),
 HValue = document.getElementById("HValue"),
 WValue = document.getElementById("WValue"),
 SRValue = document.getElementById("SRValue"),
-SSValue = document.getElementById("SSvalue"),
+SSValue = document.getElementById("SSValue"),
 CValue = document.getElementById("CValue"),
 UVValue = document.getElementById("UVValue"),
 PValue = document.getElementById("PValue"),
 Forecast = document.querySelector(".Forecast");
 
 WEATHER_API_ENDPOINT = `https://api.openweathermap.org/data/2.5/weather?appid=3b4270389f20c03b9af3d0d9bc2619ae&q=` ;
-WEATHER_DATA_ENDPOINT = `https://api.openweathermap.org/data/2.5/onecall?appid=3b4270389f20c03b9af3d0d9bc2619ae&exclude=minutely&units=metric&` ;
+WEATHER_DATA_ENDPOINT = `https://api.openweathermap.org/data/2.5/weather?appid=3b4270389f20c03b9af3d0d9bc2619ae&exclude=minutely&units=metric&` ;
 
 function findUserLocation() {
   fetch(WEATHER_API_ENDPOINT + userLocation.value)
@@ -36,11 +36,11 @@ function findUserLocation() {
     .then((data) => {
       console.log(data);
 
-      temperature.innerHTML=data.current.temp;
-      feelsLike.innerHTML="Feels like" + data.current.feels_like;
+      temperature.innerHTML=data.main.temp;
+      feelsLike.innerHTML=`Feels like ${data.main.feels_like}`;
       description.innerHTML= 
       '<i class="fa-brands fa-cloudversify"></i> &nbsp;' +
-      data.current.weather[0].description;
+      data.weather[0].description;
 
       const options = {
         weekday: 'long',
@@ -51,21 +51,21 @@ function findUserLocation() {
         hour12: true,
       }
 
-      date.innerHTML = getLongFormatDateTime(date.current.dt, date.timezone_offset, options)
+      date.innerHTML = getLongFormatDateTime(date.dt, date.timezone_offset, options)
 
-      HValue.innerHTML=Math.round(data.current.humidity)+"<span>%</span>";
-      WValue.innerHTML=Math.round(data.current.wind_speed)+"<span>m/s</span>";
+      HValue.innerHTML=Math.round(data.main.humidity)+"<span>%</span>";
+      WValue.innerHTML=Math.round(data.main.wind_speed)+"<span>m/s</span>";
       const options1 = {
         hour: 'numeric',
         minute: 'numeric',
         hour12: true,
       }
-      SRValue.innerHTML=getLongFormatDateTime(data.current.sunrise, data.timezone_offset, options1);
-      SSValue.innerHTML=getLongFormatDateTime(data.current.sunset, data.timezone_offset, options1);
+      SRValue.innerHTML=getLongFormatDateTime(data.main.sunrise, data.timezone_offset, options1);
+      SSValue.innerHTML=getLongFormatDateTime(data.main.sunset, data.timezone_offset, options1);
 
-      CValue.innerHTML=data.current.clouds+"<span>%</span>";
-      UVValue.innerHTML=data.current.uvi;
-      PValue.innerHTML=data.current.pressure+"<span>hPa</span>";
+      CValue.innerHTML=data.main.clouds+"<span>%</span>";
+      UVValue.innerHTML=data.main.uvi;
+      PValue.innerHTML=data.main.pressure+"<span>hPa</span>";
 
     });
   });
