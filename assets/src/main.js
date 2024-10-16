962bd9f0c18dab54065d31b76c65e5d3676ba51d
Nathan Scott Johnston <nathan.scott.johnston@gmail.com>
2024-10-15 18:36:36 -0700

    adding more java juice

--- assets/src/main.js
+++ assets/src/main.js
@@ -115,3 +115,82 @@ if(code == 1000) {
   app.style.backgroundImage = `
   url(/assets/images/${timeOfDay}/sunset\ cloud.jpg)`
 }
+
+let timeOfDay = "day";
+
+const code = data.current.condition.code;
+
+If(!data.current.is_day){
+  timeOfDay = "night;"
+}
+
+if(code == 1000) {
+
+
+  app.style.backgroundImage = '
+  url(./images/${timeOfDay}/clear.jpg)';
+
+
+  BigInt.style.background = "#e5ba92";
+if(timeOfDay == "night") {
+  BigInt.style.background = "#181e27";
+  }
+}
+
+else if (
+code == 1003 ||
+code == 1006 ||
+code == 1009 ||
+code == 1030
+code == 1069
+code == 1087
+code == 1135
+code == 1273
+code == 1276
+code == 1282 
+) {
+  app.style.backgroundImage = `
+  url(.images/${timeOfDay}/cloudy.jpg)`;
+  btn.style.background = "#fa6d1b";
+  if(timeOfDay == "night") {
+    btn.style.background = "#181e27";
+  }
+
+} else if (
+  code == 1063
+  code == 1069
+  code == 1072
+  code == 1150
+  code == 1153
+  code == 1180
+  code == 1183
+  code == 1189
+  code == 1192
+  code == 1195
+  code == 1204
+  code == 1207
+  code == 1240
+  code == 1243
+  code == 1246
+  code == 1249
+  code == 1252
+) {
+  app.style.backgroundImage = `
+  url(./images/${timeOfDay}/rainy.jpg)`;
+  btn.style.background = "#647d75";
+  if(timeOfDay == "night") {
+    btn.style.background = "#325c80";
+  }
+
+} else {
+  app.style.backgroundImage = `
+  url(./images/${timeOfDay}/snowy.jpg)`;
+  btn.style.background = "#4D72aa";
+  if(timeOfDay == "night") {
+    btn.style.background = "#1b1b1b";
+   }
+  }
+
+  app.style.opacity = "1";
+ })
+
